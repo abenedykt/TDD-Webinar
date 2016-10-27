fc3634fa28ea0de94ea92f58675b0a605844e035
Arkadiusz Benedykt <abenedykt@rekord.com.pl>
2016-10-27 13:14:26 +0200

    fist example

--- src/HowItWorks/Program.cs
+++ src/HowItWorks/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace HowItWorks
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace HowItWorks
 {
     internal class Program
     {
         private static void Main()
         {
-            
+            const string text = "Hello world";
+
+            var bytes = Encoding.UTF8.GetBytes(text);
+            var hashstring = new SHA256Managed();
+            var hash = hashstring.ComputeHash(bytes);
+            var sha256 = hash.Aggregate(string.Empty, (current, x) => current + $"{x:x2}");
+
+            Console.WriteLine(sha256);
         }
     }
 }
